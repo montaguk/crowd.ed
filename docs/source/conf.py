153fb4425f05cc5761585f7f6409b123fd01ca5f
Kyson Montague <kyson.montague@gmail.com>
2014-08-23 18:39:08 -0700

    Change docs theme to RTD theme, and update requirements.txt
    Change-Id: Ibf11ff56fd83ea34af97ce5ab6916879f96fdbdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,15 +106,13 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+# Use RTD Theme when building locally
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd: # Causes RTD to choke during build, so only import locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
